dcb1394d30836be90e180c1d460669ed9ab7df4f
vicSystems <victorasuquob@gmail.com>
2021-09-09 03:03:57 +0100

    db seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        $this->call(UserSeeder::class);
+        $this->call(UserTableeeder::class);
         
     }
 }
