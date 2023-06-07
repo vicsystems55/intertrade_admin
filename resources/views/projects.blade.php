b3ddacbbe3eb500fb173892b4a668cf7615fa034
vicSystems <victorasuquob@gmail.com>
2023-06-07 10:09:29 +0100

    site update

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -295,7 +295,7 @@
 							 <div class="col-md-4">
                                 <div class="card">
                                     <div class="card-body">
-                                        <h6>Benini Project - Edo State</h6>
+                                        <h6>Benin Project - Edo State</h6>
                                         <a href="/projects-reports?id=3">view details</a>
                                     </div>
                                 </div>
