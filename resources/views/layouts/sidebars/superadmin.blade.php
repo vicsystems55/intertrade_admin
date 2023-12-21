7b149107deee0ce61517d956bd12f7f74e9446b6
vicsystems <victorasuquob@gmail.com>
2023-12-21 13:01:40 +0100

    update category

--- resources/views/layouts/sidebars/superadmin.blade.php
+++ resources/views/layouts/sidebars/superadmin.blade.php
@@ -104,12 +104,12 @@
         <a href="javascript:;" class="has-arrow">
             <div class="parent-icon"><i class="bx bx-category"></i>
             </div>
-            <div class="menu-title">Products</div>
+            <div class="menu-title">Products <span class="badge bg-danger ">new</span></div>
         </a>
         <ul>
-            <li> <a href="/admin/products"><i class="bx bx-right-arrow-alt"></i>Product Catalog</a>
+            <li> <a href="/admin/products"><i class="bx bx-right-arrow-alt"></i>Product Catalog  </a>
             </li>
-            <li> <a href="/admin/product-categories"><i class="bx bx-right-arrow-alt"></i>Product Categories</a>
+            <li> <a href="/admin/product-categories"><i class="bx bx-right-arrow-alt"></i>Product Categories  </a>
             </li>
 
         </ul>
