74856b63d42c1c6d4c94673f7c905ae063e2327f
vicsystems <victorasuquob@gmail.com>
2025-05-13 16:11:40 +0100

    tor

--- resources/views/toc.blade.php
+++ resources/views/toc.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ITL Hub Privacy Policy</title>
+    <title>ITL Load Analyzer Privacy Policy</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
 </head>
@@ -12,23 +12,23 @@
 
         <div class="container pt-5">
 
-    <h1>Privacy Policy for ITL Hub</h1>
+    <h1>Privacy Policy for ITL Load Analyzer</h1>
     <p><strong>Effective Date:</strong> 20th August, 2023</p>
     <p><strong>Last Updated:</strong> 20th August, 2024</p>
 
     <h2>Introduction:</h2>
-    <p>Welcome to ITL Hub, developed by Inter-trade Ltd. This Privacy Policy is designed to inform you about our policies regarding the collection, use, and disclosure of personal data when you use our app. Your privacy is important to us, and we are committed to protecting it.</p>
+    <p>Welcome to ITL Load Analyzer, developed by Inter-trade Ltd. This Privacy Policy is designed to inform you about our policies regarding the collection, use, and disclosure of personal data when you use our app. Your privacy is important to us, and we are committed to protecting it.</p>
 
     <h2>Information We Collect:</h2>
     <ul>
         <li><strong>Personal Information:</strong> We may collect personal information, such as your name, email address, phone number, and billing information, as necessary for providing our services, including hub management and transaction processing.</li>
-        <li><strong>Usage Information:</strong> We collect information about how you use ITL Hub, including the features you access and the actions you take within the app, to improve our services.</li>
+        <li><strong>Usage Information:</strong> We collect information about how you use ITL Load Analyzer, including the features you access and the actions you take within the app, to improve our services.</li>
         <li><strong>Device Information:</strong> We may collect information about your device, including its unique identifiers, operating system, and mobile network information, for the purpose of ensuring app compatibility.</li>
     </ul>
 
     <h2>How We Use Your Information:</h2>
     <ul>
-        <li><strong>Service Delivery:</strong> We use your personal information to provide you with the ITL Hub app's features and functionalities, including hub management and transaction processing.</li>
+        <li><strong>Service Delivery:</strong> We use your personal information to provide you with the ITL Load Analyzer app's features and functionalities, including hub management and transaction processing.</li>
         <li><strong>Communication:</strong> We may use your contact information to communicate with you about your account, updates, and important information related to the app.</li>
         <li><strong>Improvement:</strong> We analyze usage data to enhance the app's performance, features, and user experience.</li>
         <li><strong>Legal Compliance:</strong> We may use your information to comply with legal obligations and respond to law enforcement requests.</li>
@@ -36,7 +36,7 @@
 
     <h2>Sharing Your Information:</h2>
     <ul>
-        <li><strong>Third-party Service Providers:</strong> We may engage third-party service providers to assist us in delivering the ITL Hub app. These providers are obligated to protect your information.</li>
+        <li><strong>Third-party Service Providers:</strong> We may engage third-party service providers to assist us in delivering the ITL Load Analyzer app. These providers are obligated to protect your information.</li>
         <li><strong>Legal Authorities:</strong> We may share your information when required by law or to protect our rights, privacy, safety, or property or that of others.</li>
     </ul>
 
@@ -56,12 +56,12 @@
     </ul>
 
     <h2>Contact Us:</h2>
-    <p>If you have any questions or concerns about our Privacy Policy or the data practices of ITL Hub, please contact us at <a href="mailto:support@intertrade.com">support@intertrade.com</a>.</p>
+    <p>If you have any questions or concerns about our Privacy Policy or the data practices of ITL Load Analyzer, please contact us at <a href="mailto:support@intertrade.com">support@intertrade.com</a>.</p>
 
     <h2>Changes to this Privacy Policy:</h2>
     <p>We may update this Privacy Policy to reflect changes to our data practices. We will notify you of any changes by posting the new Privacy Policy on this page.</p>
 
-    <p>Thank you for using ITL Hub!</p>
+    <p>Thank you for using ITL Load Analyzer!</p>
 
     <p>Inter-trade Ltd.</p>
     <p>Plot 798, Olajide Laleye Street, Jahi Abuja</p>
